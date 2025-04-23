a45a787777158662154630e098539c150a5830ee
somesh shinde <shindesomesh@gmail.com>
2025-04-23 12:39:02 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,2 +1,11 @@
-const express= require('express');
-const app= express();
+const express = require('express');
+const app = express();
+const taskRoutes = require('./routes/app.routes');
+const cors = require('cors');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/api/v1/', taskRoutes);
+
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const db=require('./utils/db')
+const app = require('./app');
+
+db.sync({force:false}).then(()=>{
+    console.log('Database Synced Successfully')
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => {
+        console.log(`Server Started on http://localhost:${port}`)
+    });
+}).catch((err)=>{
+    console.log('Database Sync Failed',err)
+})
